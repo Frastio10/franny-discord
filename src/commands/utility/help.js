49e775436e0ce8459530ab6c4ad51e4d73fa9062
Frastio Agustian <43699753+Frastio10@users.noreply.github.com>
2020-12-24 13:23:30 +0700

    removed some comments

--- src/commands/utility/help.js
+++ src/commands/utility/help.js
@@ -16,21 +16,15 @@ module.exports = class HelpCommand extends Commando.Command{
 		const helpEmbed = new MessageEmbed()
 			.setColor('#0099ff')
 			.setTitle('All Commands')
-			// .setURL('https://discord.gg/')
 			.setAuthor('Franny', botIcon )
 			.setDescription('Here are the commands that you can use! \n `Prefix: f.`')
-			//.setThumbnail('https://i.imgur.com/PGrtSUO_d.webp')
 			.addFields(
 
 				{ name: 'Server Management', value: '`kick`,`ban`,`mute`' },
 				{ name: 'Games', value: '`bla`,`bla`,`nye`' },
 				{ name: 'Utility', value: '`ping`' },
 				{ name: '\u200B', value: '\u200B' },
-				// { name: 'Inline field title', value: 'Some value here', inline: true },
-				// { name: 'Inline field title', value: 'Some value here', inline: true },
 			)
-			//.addField('Inline field title', 'Some value here', true)
-				
 			.setTimestamp()
 			.setFooter('Thanks for using Franny!', botIcon);
 			message.channel.send(helpEmbed);
